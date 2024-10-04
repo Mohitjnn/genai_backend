02b8ec1d05ca139d995fcd7e30b7dfcf6cc9ce65
Mohitjnn <jnmohit29@gmail.com>
2024-10-05 03:33:54 +0530

    initial commit routes prepared

---
+++ MachineLearning/test.py
@@ -0,0 +1,2 @@
+def process_file_and_text(file: bytes, file_name: str, text: str):
+    return "response text"
---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from MachineLearning.test import process_file_and_text
+
+app = FastAPI()
+
+
+# Route for uploading only files (file is now required)
+@app.post("/fileUpload/")
+async def upload_file_route(file: UploadFile = File(...)):  # File is now required
+    files_content = await file.read()
+    result = process_file_and_text(files_content, file.filename, "")
+    return result
+
+
+# Route for submitting only text (description is now required)
+@app.post("/text/")
+async def submit_text_route(description: str = Form(...)):  # Text is now required
+    result = process_file_and_text(b"", "", description)
+    return result
+
+
+# Route for submitting both file and text (both are required)
+@app.post("/file_and_text/")
+async def upload_file_and_text_route(
+    file: UploadFile = File(...),  # File is now required
+    description: str = Form(...),  # Text is now required
+):
+    files_content = await file.read()
+    result = process_file_and_text(files_content, file.filename, description)
+    return result
