a92f2bd13d37bbdd75262467c9c4388045559dd3
Mohitjnn <jnmohit29@gmail.com>
2024-10-06 01:11:05 +0530

    cors set up and connect to frontend

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from MachineLearning.test import process_file_and_text
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://192.168.29.147:3000", #replace with your own ipv4 address:3000
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Route for uploading only files (file is now required)
-@app.post("/fileUpload/")
-async def upload_file_route(file: UploadFile = File(...)):  # File is now required
+@app.post("/fileUpload")
+async def upload_file_route(file: UploadFile = File(...)):
     files_content = await file.read()
     result = process_file_and_text(files_content, file.filename, "")
     return result
 
 
 # Route for submitting only text (description is now required)
-@app.post("/text/")
+@app.post("/text")
 async def submit_text_route(description: str = Form(...)):  # Text is now required
     result = process_file_and_text(b"", "", description)
     return result
